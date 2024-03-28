bf354f93779376de50ec31083aca7af01eafe4e9
Sandeep K <79915877+SandeepUpadhyaya@users.noreply.github.com>
2024-03-28 21:39:51 +0530

    prepare base model is added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sandeep_deep_learning_project import logger
-from sandeep_deep_learning_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from sandeep_deep_learning_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from sandeep_deep_learning_project.pipeline.stage_01_data_ingestion import *
+from sandeep_deep_learning_project.pipeline.stage_02_prepare_base_model import *
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -8,6 +9,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
